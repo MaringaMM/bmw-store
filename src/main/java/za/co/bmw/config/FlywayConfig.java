d18714ec164f6bd64117cb941ba064956a08ca3b
Musa Marvin Maringa <cube.musa@gmail.com>
2023-04-20 08:35:14 +0200

    Flyway bean injection

--- src/main/java/za/co/bmw/config/FlywayConfig.java
+++ src/main/java/za/co/bmw/config/FlywayConfig.java
@@ -1,11 +1,13 @@
 package za.co.bmw.config;
 
 import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FlywayConfig {
 
+    @Bean
     public void configureFlyway(){
         Flyway flyway = Flyway.configure()
                 .dataSource("jdbc:sqlite/store.sqlite", "", "")
